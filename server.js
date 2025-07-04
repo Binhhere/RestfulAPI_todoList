2ec320b96db6b03a100f0089d8adff39bb4a659b
Binhhere <binh.trankhanh0503@gmail.com>
2025-07-04 13:05:51 +0700

    update 4/7/2025

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const colors = require('colors');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); 
+}
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Port hear
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.MODE} mode on port ${process.env.PORT}`);
+});
+
