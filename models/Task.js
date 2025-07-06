977a8575db933b997e5c480141458a4aeb1aebc8
Binhhere <binh.trankhanh0503@gmail.com>
2025-07-07 00:10:19 +0700

    update code

---
+++ models/Task.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title"],
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    dueDate: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in progress", "completed"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", TaskSchema);
---
+++ models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+// user
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+// bcrypt password
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// check password before login
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", UserSchema);
