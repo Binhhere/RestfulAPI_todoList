cf47e8dbde5c3f10e36a7b9b2babdf99b0a00aa5
Binhhere <binh.trankhanh0503@gmail.com>
2025-07-08 09:04:07 +0700

    delete duplicated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,54 +1,67 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-// POST: Register new user
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+// register
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
+
   try {
-    //check duplicate email
-    const existingUser = await User.findOne({ email });
-    if (existingUser)
-      return res.status(400).json({ mesaage: "Email already exists" });
+    const userExists = await User.findOne({ email });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPass = await bcrypt.hash(password, salt);
 
-    //save new User
-    const newUser = await User.create({
+    const user = await User.create({
       name,
       email,
-      password: hashedPassword,
+      password: hashedPass,
     });
-    res
-      .status(201)
-      .json({ message: "User register successfully", user: newUser });
-  } catch (err) {
-    res.status(500).json({ error: "Server error" });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Registration failed" });
   }
 };
 
-// POST: Login user
+// login
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.findOne({ email });
-    const isMatch = user
-      ? await bcrypt.compare(password, user.password)
-      : false;
-    if (!user || !isMatch) {
-      return res
-        .status(400)
-        .json({ message: "Invalid email or password. Please try again." });
-    }
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
     });
-    res.status(200).json({ token, user: { id: user._id, name: user.name } });
-  } catch (err) {
-    res.status(500).json({ error: "Server error" });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed" });
   }
 };
 
-module.exports = { registerUser, loginUser };
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const setupSwagger = require("./swagger");
 const userRoutes = require("./routes/userRoutes");
+const globalErrorHandler = require("./middleware/globalErrorHandler");
 
 dotenv.config();
 
@@ -28,14 +29,23 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
+// Swagger
 setupSwagger(app);
+
+// 404 Handler 
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// Port hear
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.MODE} mode on port ${PORT}`);
